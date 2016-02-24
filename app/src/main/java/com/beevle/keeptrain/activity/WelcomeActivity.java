f38ab856d268547e6639a2930441eca75ef41abb
kangsafe <kangsafe@hotmail.com>
2016-02-25 00:29:05 +0800

    登陆页面

--- app/src/main/java/com/beevle/keeptrain/activity/WelcomeActivity.java
+++ app/src/main/java/com/beevle/keeptrain/activity/WelcomeActivity.java
@@ -1,16 +1,36 @@
 package com.beevle.keeptrain.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.beevle.keeptrain.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private Button btnReg;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        //
+        btnReg=(Button)findViewById(R.id.btn_welcome_register);
+        btnLogin=(Button)findViewById(R.id.btn_welcome_login);
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(WelcomeActivity.this,LoginActivity.class);
+                WelcomeActivity.this.startActivity(intent);
+                WelcomeActivity.this.finish();
+            }
+        });
     }
 }
