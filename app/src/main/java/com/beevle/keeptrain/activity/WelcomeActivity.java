5b54152c50abe4df069374dc12a08ecb79c13976
kangsafe <kangsafe@hotmail.com>
2016-02-25 22:50:01 +0800

    登陆页面

--- app/src/main/java/com/beevle/keeptrain/activity/WelcomeActivity.java
+++ app/src/main/java/com/beevle/keeptrain/activity/WelcomeActivity.java
@@ -7,34 +7,17 @@ import android.view.View;
 import android.widget.Button;
 
 import com.beevle.keeptrain.R;
+import com.beevle.keeptrain.model.BwToken;
 import com.beevle.keeptrain.model.UserToken;
+import com.beevle.keeptrain.service.ApiService;
 
-public class WelcomeActivity extends AppCompatActivity {
+import butterknife.Bind;
 
-    private Button btnReg;
-    private Button btnLogin;
-//    private ApiService.TokenCallback callback = new ApiService.TokenCallback() {
-//        @Override
-//        public void onSuccess(BwToken bwToken) {
-//            if (bwToken.getErrcode() == 0) {
-//                String AccessToken=bwToken.getData().getAccess_token();
-//                Long Timeout=bwToken.getData().getExpires_in() - 20 + new Date().getTime();
-//                SpUtil.setStringSharedPerference(mySharedPreferences,
-//                        PrefsConsts.PREFS_DDSCHOOL_TOKEN_ACCESSTOKEN,
-//                        AccessToken
-//                );
-//                SpUtil.setLongSharedPerference(mySharedPreferences,
-//                        PrefsConsts.PREFS_DDSCHOOL_TOKEN_TIMEOUT,
-//                        Timeout
-//                );
-//            }
-//        }
-//
-//        @Override
-//        public void onError() {
-//
-//        }
-//    };
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener, ApiService.TokenCallback {
+    @Bind(R.id.btn_welcome_register)
+    Button btnReg;
+    @Bind(R.id.btn_welcome_login)
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,19 +28,36 @@ public class WelcomeActivity extends AppCompatActivity {
         //ApiService.getToken(callback);
         //UserToken.getInstance(getApplicationContext())
         UserToken.getToken(getApplicationContext());
-        btnReg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
 
-            }
-        });
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
-                WelcomeActivity.this.startActivity(intent);
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_welcome_login:
+                Intent login = new Intent(WelcomeActivity.this, LoginActivity.class);
+                WelcomeActivity.this.startActivity(login);
                 WelcomeActivity.this.finish();
-            }
-        });
+                break;
+            case R.id.btn_welcome_register:
+                Intent reg = new Intent(WelcomeActivity.this, RegisterActivity.class);
+                WelcomeActivity.this.startActivity(reg);
+                WelcomeActivity.this.finish();
+                break;
+        }
+    }
+
+    @Override
+    public void onSuccess(BwToken bwToken) {
+
+    }
+
+    @Override
+    public void onError() {
+
     }
 }
